c37d5c322215fd43ea1c249e445a7f1274f1673b
jonas.braha <jonas.braha@infis.cz>
2023-11-02 13:08:59 +0100

    cviceni

---
+++ src/cvika/Arrays.java
@@ -0,0 +1,20 @@
+package cvika;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] pole = {5, 32, 21, 15};
+        int[] poleJinak = new int[10];
+        System.out.println(pole[2]);
+        for (int i = 0; i < pole.length; i++) {
+            System.out.println(pole[i]);
+        }
+        pole[2] = 10;
+        for (int i = 0; i < poleJinak.length; i++) {
+            poleJinak[i] = (int) (Math.random() * 101);
+        }
+        System.out.println("---");
+        for (int i = 0; i < poleJinak.length; i++) {
+            System.out.println(poleJinak[i]);
+        }
+    }
+}
