d06844f3ee76dad35eb5c5696adfeb988e5c7784
jonas.braha <jonas.braha@infis.cz>
2023-12-01 12:17:56 +0100

    zaklady

--- src/Strings/Basics.java
+++ src/Strings/Basics.java
@@ -4,32 +4,54 @@ import java.util.Scanner;
 
 public class Basics {
     public static void main(String[] args) {
-        char letter = 'A';
-        char num = '9';
+        char letter = 'A'; //muzou se tam dat pismena
+        char num = '9';  //cisla
         char OtherNum = '5';
-        char other = '*';
-  //      char specials = -' ';   //mezera
-        char LineBreak = '\n';  //odradkovani
+        char other = '*'; //ostatni jako *,_
+        char special = '\n';  //odradkovani
         char lineBreak = '\t';  //tabulator
-        System.out.println("Hello" + LineBreak + "World");
+
+        System.out.println("Hello" + special + "World");
         System.out.println(OtherNum + num);
         Scanner sc = new Scanner(System.in);
 
+  //vypis hello world
         String word = "Hello";
         String otherWord = "World!";
         System.out.println(word + " " + otherWord);
+
+  //vypis ahoj svete + delka slov
+        String slovo = "Ahoj svete";
+        System.out.println(slovo);
+        System.out.println("Delka je: " + slovo.length());
 
         String sumthing = "neco";
         System.out.println("Zadej slovo");
         String somethingElse = sc.nextLine();
         System.out.println("Zadal jsi: " + somethingElse);
-        //porovnani
+        System.out.println("-----------------------");
+
+ //i     String slovo2 ="Zdravim";
+ //d     if (slovo2.contains("ra"))
+ //k        System.out.println("Obsahuje 'ra' ");
+ //      int indexE = slovo2.indexOf('z');
+ //      if (indexE >= 0) {
+ //          System.out.println("'z' se nachazi na indexu: " + indexE);
+ //      }
+
+  //porovnani
         if (sumthing.equals(somethingElse)) {
-            System.out.println("They're same");
+            System.out.println("Jsou shodne.");
+        } else {
+            System.out.println("Nejsou shodne.");
         }
-        System.out.println("Druhy pismeno je: " + sumthing.charAt(2));
-        System.out.println(sumthing.toUpperCase());
-        System.out.println(sumthing);
+        System.out.println("-----------------------");
+
+  //nacteni pismenka ze slova (druhe pismeno), ktere napiseme
+        System.out.println("Druhy pismeno je: " + somethingElse.charAt(2));
+        System.out.println("-----------------------");
+        System.out.println("Slovo velkymi pismeny: " +somethingElse.toUpperCase());
+        System.out.println();
 
 
 
